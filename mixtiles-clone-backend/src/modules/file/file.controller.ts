18f3d330805ae481d7a72e0cdc129b1157b53d1d
techaks <akkuba53@gmail.com>
2020-12-24 19:10:30 +0530

    backend fix

--- mixtiles-clone-backend/src/modules/file/file.controller.ts
+++ mixtiles-clone-backend/src/modules/file/file.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { FileService } from './file.service';
 import { CreateFileDto } from './dto/create-file.dto';
 import { GetUserId } from '../user/get-user.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('files')
 @UseGuards(AuthGuard('jwt'))
@@ -13,6 +21,13 @@ export class FileController {
 
   @Post('create')
   create(@GetUserId() userId: string, @Body() createDto: CreateFileDto) {
+    console.log(createDto);
     return this.fileService.createFile(userId, createDto);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file) {
+    console.log(file);
   }
 }
